d8befabe3b99ff82ddd93b789e2f64e96ad7c75a
Julian Contreras <jjulian.contrerass@gmail.com>
2026-03-29 21:04:30 -0300

    Subo lo de matias: Getters y Setters de los atributos y el método toString de persona y lo mio: Primer y segundo constructor para Empleado

--- src/EJERCICIO01/Empleado.java
+++ src/EJERCICIO01/Empleado.java
@@ -2,17 +2,39 @@ package EJERCICIO01;
 
 import java.time.LocalDate;
 
-public class Empleado extends Persona{
-
-	
+public class Empleado extends Persona
+{
 	private int legajo;
 	private String puesto; 
 	
 	//Constructores	
 
-	public Empleado() {};
-	public Empleado(String dni,String nombre, String apellido, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email, String puesto) {};
+	public Empleado() {
+		super();
+		this.puesto = "sin puesto";
+	}
+
+	public Empleado(String dni, String nombre, String apellido, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email, String puesto) {
+		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+		this.puesto = puesto;
+	}
 	
 	//Getters y Setters
 	
+	public int getLegajo() {
+		return legajo;
+	}
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
+	public String getPuesto() {
+		return puesto;
+	}
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	@Override
+	public String toString() {
+		return "Empleado [Legajo=" + legajo +  ", Puesto=" + puesto +  "]" + ", " + super.toString();
+	}
 }
