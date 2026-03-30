66c5ebd0c343eb08b06298a6baf886ae05cdafe2
nemi1414 <juanignacionemi@gmail.com>
2026-03-30 08:50:42 -0300

    implementé legajo como constante y contador estático de legajos

--- src/EJERCICIO01/Empleado.java
+++ src/EJERCICIO01/Empleado.java
@@ -4,18 +4,21 @@ import java.time.LocalDate;
 
 public class Empleado extends Persona
 {
-	private int legajo;
+	private final int legajo; //Ahora es constante
 	private String puesto; 
+	private static int contadorLegajos = 1000; //hacemos que inicie en 1000
 	
 	//Constructores	
 
 	public Empleado() {
 		super();
+		this.legajo = contadorLegajos++; //implementamos el contador de legajos
 		this.puesto = "sin puesto";
 	}
 
 	public Empleado(String dni, String nombre, String apellido, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email, String puesto) {
 		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+		this.legajo = contadorLegajos++; // implementamos el contador de legajos
 		this.puesto = puesto;
 	}
 	
@@ -24,9 +27,9 @@ public class Empleado extends Persona
 	public int getLegajo() {
 		return legajo;
 	}
-	public void setLegajo(int legajo) {
+	/*public void setLegajo(int legajo) {  quito este setLegajo ya que ahora es constante
 		this.legajo = legajo;
-	}
+	}*/
 	public String getPuesto() {
 		return puesto;
 	}
